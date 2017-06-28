9692f36c23f6defb46e5e306102709ac5834a49c
kamir222 <katia.amir@gmail.com>
2017-06-28 19:15:12 +0000

    initiate connection in react app to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -33,16 +33,9 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    //connect React app to the server (initiate the connection)
+    this.socket = new WebSocket("ws://localhost:5657");
+    console.log('Connected to server');
   }
 
 
